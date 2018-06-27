d18efd5ac1739754da9f99b69ac8bff46770eea6
Daniel Kang <danielkang674@gmail.com>
2018-06-27 14:27:42 -0500

    "Detect Discover card digits"

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,41 +13,65 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
   let firstTwoDigits = cardNumber[0] + cardNumber[1];
   let firstOneDigit = cardNumber[0];
+  let firstFourDigits = cardNumber[0] + cardNumber [1] + cardNumber[2] + cardNumber[3];
+  let firstThreeDigits = cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3];
+  const discoverThreeDigits = ['644', '645', '646', '647', '648', '649'];
+  const masterTwoDigits = ['51', '52', '53', '54', '55'];
+  function checkDigits(array, numbers){
+    for(let value of array){
+      if(value === numbers){
+        return true;
+      }
+    }
+    return false;
+  }
+
   if(cardNumber.length === 14){
     if(firstTwoDigits === '38' || firstTwoDigits === '39'){
       return 'Diner\'s Club';
     }
   }
+
   if(cardNumber.length === 15){
     if(firstTwoDigits === '34' || firstTwoDigits === '37'){
       return 'American Express';
     }
   }
+
   if(cardNumber.length === 13){
     if(firstOneDigit === '4'){
       return 'Visa';
     }
   }
+
   if(cardNumber.length === 16){
     if(firstOneDigit === '4'){
       return 'Visa';
-    } else if(firstTwoDigits === '51'){
-      return 'MasterCard';
-    } else if(firstTwoDigits === '52'){
-      return 'MasterCard';
-    } else if(firstTwoDigits === '53'){
-      return 'MasterCard';
-    } else if(firstTwoDigits === '54'){
-      return 'MasterCard';
-    } else if(firstTwoDigits === '55'){
+    }
+    if(checkDigits(masterTwoDigits, firstTwoDigits)){
       return 'MasterCard';
     }
+    if(firstFourDigits === '6011' || firstTwoDigits === '65'){
+      return 'Discover';
+    }
+    if(checkDigits(discoverThreeDigits, firstThreeDigits)){
+      return 'Discover';
+    }
   }
+  
   if(cardNumber.length === 19){
     if(firstOneDigit === '4'){
       return 'Visa';
     }
+    if(firstFourDigits === '6011' || firstTwoDigits === '65'){
+      return 'Discover';
+    }
+    if(checkDigits(discoverThreeDigits, firstThreeDigits)){
+      return 'Discover';
+    }
   }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
+
+
 
