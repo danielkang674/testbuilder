55d9848914b5ee9e7c13d5f964e9f3776fa1f2a0
Daniel Kang <danielkang674@gmail.com>
2018-06-27 09:56:19 -0500

    "Add MasterCard detection and array"

--- detectNetwork.js
+++ detectNetwork.js
@@ -12,6 +12,7 @@ var detectNetwork = function(cardNumber) {
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
   let firstTwoDigits = cardNumber[0] + cardNumber[1];
+  const MCDIGITS = ['51', '52', '53', '54', '55']
   if(cardNumber.length === 14 && firstTwoDigits === '38' || firstTwoDigits === '39'){
     return 'Diner\'s Club';
   }
@@ -22,7 +23,14 @@ var detectNetwork = function(cardNumber) {
     return 'Visa';
   }
   if(cardNumber.length === 16){
-    if()
+    if(cardNumber[0] === '4'){
+      return 'Visa';
+    }
+    for(let i = 0; i < MCDIGITS.length; i++){
+      if(firstTwoDigits === MCDIGITS[i]){
+        return 'MasterCard';
+      }
+    }
   }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
