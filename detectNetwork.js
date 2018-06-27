bd78b0f166691aca2835ab2c8666bbfa5f1bc723
Daniel Kang <danielkang674@gmail.com>
2018-06-27 09:51:04 -0500

    "Add a variable to access front digits easier"

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,14 +11,18 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-  if(cardNumber.length === 14 && cardNumber[0] + cardNumber[1] === '38' || cardNumber[0] + cardNumber[1] === '39'){
+  let firstTwoDigits = cardNumber[0] + cardNumber[1];
+  if(cardNumber.length === 14 && firstTwoDigits === '38' || firstTwoDigits === '39'){
     return 'Diner\'s Club';
   }
-  if(cardNumber.length === 15 && cardNumber[0] + cardNumber[1] === '34' || cardNumber[0] + cardNumber[1] === '37'){
+  if(cardNumber.length === 15 && firstTwoDigits === '34' || firstTwoDigits === '37'){
     return 'American Express';
   }
   if(cardNumber[0] === '4' && cardNumber.length === 13 || cardNumber.length === 19){
     return 'Visa';
+  }
+  if(cardNumber.length === 16){
+    if()
   }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
